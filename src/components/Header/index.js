91b9282a5c7fa43baee78b3570660e77b4c07ad0
nasir03082409229 <nasir03082409229@gmail.com>
2019-10-23 19:01:43 +0500

    Header Code Refactor - [Update]

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import AppBar from 'material-ui/AppBar';
 import RaisedButton from 'material-ui/RaisedButton';
 import configs from './../../configs/config';
@@ -6,22 +6,22 @@ import PropTypes from 'prop-types';
 
 class Header extends Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.rightButton = this.rightButton.bind(this);
-    this.leftButton = this.leftButton.bind(this);
   }
 
-  rightButton(props){
-    if(props.onClick)
-      return <RaisedButton label="Add" secondary={true} style={{marginTop: '6px', marginRight: '6px'}} onClick={props.onClick} />;
-    return <span/>;
+  rightButton = (props) => {
+    const { onClick } = props
+    if (onClick)
+      return <RaisedButton label="Add" secondary={true} style={{ marginTop: '6px', marginRight: '6px' }} onClick={props.onClick} />;
+    return <span />;
   }
 
-  leftButton(props){
-    if(props.onClick)
-      return <RaisedButton label="Some left button" secondary={true} style={{marginTop: '6px', marginRight: '6px'}} />;
-    return <span/>;
+  leftButton = (props) => {
+    const { onClick } = props
+    if (onClick)
+      return <RaisedButton label="Some left button" secondary={true} style={{ marginTop: '6px', marginRight: '6px' }} />;
+    return <span />;
   }
 
   render() {
@@ -30,7 +30,7 @@ class Header extends Component {
         <AppBar
           title={configs.APP_TITLE}
           iconElementLeft={this.leftButton({})}
-          iconElementRight={this.rightButton({onClick: this.props.onRightClick})}
+          iconElementRight={this.rightButton({ onClick: this.props.onRightClick })}
         />
       </div>
     );
